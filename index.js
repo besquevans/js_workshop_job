a544769f28ee84be946443143b23e46a1bbdff7d
Gavin Liu <du20783@gmail.com>
2020-08-25 14:22:57 +0800

    render job html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  const jobList = document.querySelector("#job-pannel")
   ///toggle navbar-menu
   document.querySelector("#navbar-burger").addEventListener("click", () => {
     document.querySelector("#navbar-menu").classList.toggle('is-active');
@@ -10,11 +11,33 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault()
     const description = searchForm.description.value
     const location = searchForm.location.value
-    const full_time = searchForm.full_time.value
+    let full_time = ""
+    if(searchForm.full_time.checked){
+      full_time = "on"
+    }
 
     const url = 'https://still-spire-37210.herokuapp.com/positions.json'
+    jobList.innerHTML = ''
+
     fetch(`${url}?description=${description}&location=${location}&full_time=${full_time}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then((data) => {
+      console.log(data)
+      data.forEach((job) => createJob(job))
+    })
   })
+
+
+  ///render search result
+  function createJob(job) {
+    const t = document.querySelector("#search-result-item")
+    const clone = document.importNode(t.content, true)
+    clone.querySelector(".company").href = job.company_url
+    clone.querySelector(".company").textContent = job.company
+    clone.querySelector("h4 a").href = job.url
+    clone.querySelector("h4 a").textContent = job.title
+    clone.querySelector(".fulltime").textContent = job.type
+    clone.querySelector(".location").textContent = job.location
+    jobList.appendChild(clone)
+  }
 })
